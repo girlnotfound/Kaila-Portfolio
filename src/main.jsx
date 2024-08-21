e5f366890019a8def8cac88fe2cf6aae28d3e05e
girlnotfound <150276019+girlnotfound@users.noreply.github.com>
2024-08-21 13:19:03 -0700

    Updated main.jsx to add routing and Bootstrap styling

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/style.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+// component imports
+import App from "./App";
+import ErrorPage from "./pages/Error";
+import About from "./pages/AboutMe";
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/portfolio";
+import Resume from "./pages/Resume";
+
+// router configuration
+const routerConfig = [
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <About /> },
+      { path: "/About", element: <About /> },
+      { path: "/Contact", element: <Contact /> },
+      { path: "/Portfolio", element: <Portfolio /> },
+      { path: "/Resume", element: <Resume /> },
+    ],
+  },
+];
+
+const router = createBrowserRouter(routerConfig);
+
+// render the app
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<RouterProvider router={router} />);
